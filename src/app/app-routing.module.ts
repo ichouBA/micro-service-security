a4fc2855d25fdf3a70908f8ef2d747732eb22a1f
ichou <ichou.bahba@gmail.com>
2024-02-27 21:34:06 +0100

    guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {ProductsComponent} from "./products/products.component";
 import {CustomerComponent} from "./customer/customer.component";
+import {AuthGuard} from "./guards/auth.guard";
 
 const routes: Routes = [
-  {path: "products", component: ProductsComponent},
-  {path: "customers", component: CustomerComponent}
+  {path: "products", component: ProductsComponent, canActivate: [AuthGuard], data: {roles:["USER"]}},
+  {path: "customers", component: CustomerComponent, data: {roles:["ADMIN"]}}
 ];
 
 @NgModule({
